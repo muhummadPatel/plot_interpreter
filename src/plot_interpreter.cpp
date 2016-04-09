997313aed10285517c85854d66703deb0243ad6a
Muhummad Patel <muhummad.patel@gmail.com>
2016-04-09 19:25:07 +0200

    Add origin command and ack all commands with 'x'.

--- src/plot_interpreter.cpp
+++ src/plot_interpreter.cpp
@@ -15,17 +15,26 @@ void cmd_line(Shell &shell, int argc, const ShellArguments &argv){
   long start[] = {atol(argv[1]), atol(argv[2])};
   long end[] = {atol(argv[3]), atol(argv[4])};
   artist.line(start, end);
+
+  shell.write('x');
+}
+
+void cmd_origin(Shell &shell, int argc, const ShellArguments &argv){
+  artist.origin();
+
+  shell.write('x');
 }
 
 void setup(){
   Serial.begin(9600);
-  shell.setPrompt("$");
+  shell.setPrompt("$\n");
   shell.begin(Serial);
 
   pinMode(3, OUTPUT);
   digitalWrite(3, LOW);
 
   ShellCommand(line, "draw a line", cmd_line);
+  ShellCommand(origin, "move pen to origin", cmd_origin);
 }
 
 void loop(){
